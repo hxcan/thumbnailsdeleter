537b41641646b8e540e032bd851c5c4f1104043e
Hxcan Cai <caihuosheng@gmail.com>
2021-08-31 09:38:38 +0800

    initialCommit

---
+++ CommonHeader.h
@@ -0,0 +1,12 @@
+#ifndef COMMONHEADER_H
+#define COMMONHEADER_H
+
+/*!
+ *这里包含一些自己写的常用的头文件。
+ */
+
+#include "DataType.h" //StoreNameCard
+#include "ThumbnailsDeleter.h" //
+#include "MainLogic.h" //DictItem
+
+#endif // COMMONHEADER_H
